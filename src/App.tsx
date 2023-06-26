0d860dd65b722a38bd3a1e23ff51bf55d071d941
rajpatelbot <rajpatel158g@gmail.com>
2023-06-26 15:40:37 +0530

    feat: Implemented the abortcontroller

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./App.css";
-import Demo from "./hooks/useFetch/Demo";
+import Fetch from "./hooks/useFetch/Demo";
+import Copy from "./hooks/useCopyToClipboard/Demo";
 
 function App() {
+  const [state, setState] = useState("copy");
+
   return (
     <div>
       <p>Custom hook</p>
-      <Demo />
+      <button onClick={() => setState("fetch")}>useFetch</button>
+      <button onClick={() => setState("copy")}>useCopyToClipboard</button>
+
+      {state === "fetch" && <Fetch />}
+      {state === "copy to clipboard" && <Copy />}
     </div>
   );
 }
